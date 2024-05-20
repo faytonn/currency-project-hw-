b738d00b68b040109ac8c00f2a40bbe6353d968d
Fatima <fatimavaliyeva0704@gmail.com>
2024-05-20 16:11:32 +0400

    exit and the rest commits made

--- currency project/Program.cs
+++ currency project/Program.cs
@@ -37,6 +37,7 @@ namespace currency_project
 
                 }
 
+
                 else if (command == "2")
                 {
                     Console.WriteLine("Please write a currency code (USD, RUB, TRY)");
@@ -58,39 +59,56 @@ namespace currency_project
                     {
                         Console.WriteLine("Invalid code written.");
                     }
-                    else if (command == "3")
+                }
+                else if (command == "3")
+                {
+
+                    Console.WriteLine("Please write a valid amount");
+                    decimal amount = Decimal.Parse(Console.ReadLine());
+                    if (amount >= 0)
                     {
+                        Console.WriteLine("Please enter a code of currency (USD, RUB, TRY)");
+                        string currency_code2 = Console.ReadLine();
 
-                        Console.WriteLine("Please write a valid amount");
-                        decimal amount = Decimal.Parse(Console.ReadLine());
-                        if (amount >= 0)
+                        bool found = false;
+                        while (i < currencyCodes.Length)
                         {
-                            Console.WriteLine("Please enter a code of currency (USD, RUB, TRY)");
-                            string currency_code2 = Console.ReadLine();
+                            if (currencyCodes[i] == currency_code2)
+                            {
+                                decimal rate = currencyRates[i];
+                                decimal amountConversion = rate * amount;
+                                Console.WriteLine($"Entered amount is {amountConversion} AZN");
+                                found = false;
+                                break;
+                            }
+                            i++;
+                        }
+                        if (!found)
+                        {
+                            Console.WriteLine("Invalid currency written.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid amount entered.");
+                    }
 
-                            bool found = false;
-                            while (i < currencyCodes.Length)
-                            {
-                                if (currencyCodes[i] == currency_code2)
-                                {
-                                    decimal rate = currencyRates[i];
-                                    decimal amountConversion = rate * amount;
-                                    Console.WriteLine($"Entered amount is {amountConversion} AZN");
-                                    found = false;
-                                    break;
-                                }
-                                i++;
-                            }
-                            if (!found)
-                            {
-                                Console.WriteLine("Invalid currency written.");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid amount entered.");
-                        }
-                        
-            }           
+
+                }
+                else if (command == "4")
+                {
+                    systemProcess = false;
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("The command you entered is not available");
+                }
+
+
+
+            }
+        }           
     }
 }
