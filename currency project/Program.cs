d62f842a7a3e69a2823ae13d38b38b3b33f42a44
Fatima <fatimavaliyeva0704@gmail.com>
2024-05-20 16:07:03 +0400

    Showing whole currencies command was completed

--- currency project/Program.cs
+++ currency project/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            
-        }
+            string[] currencyCodes = { "USD", "RUB", "TRY" };
+            decimal[] currencyRates = { 1.70M, 0.0187M, 0.0527M };
+            bool systemProcess = true;
+
+            int i = 0;
+
+
+
+            while (systemProcess)
+            {
+
+                Console.WriteLine("Available commands:");
+                Console.WriteLine("1. /show-recent-currency-rates");
+                Console.WriteLine("2. /find-currency-rate-by-code");
+                Console.WriteLine("3. /calculate-amount-by-currency");
+                Console.WriteLine("4. /exit");
+                Console.Write("Please enter a valid command: ");
+                string command = Console.ReadLine();
+
+
+                if (command == "1")
+                {
+                    Console.WriteLine("Current currency rates: ");
+                    while (i < currencyCodes.Length)
+                    {
+                        Console.WriteLine($"{currencyCodes[i]} : {currencyRates[i]}");
+                        i++;
+                    }
+
+                }
+            }
     }
 }
